8577806d18c5ec2f72977d993e2b2da2f8019e1e
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-14 18:15:56 +0100

    Finito snack 6 *fine

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,3 +1,20 @@
+let numCPU = Math.floor(Math.random() * 10);
+document.getElementById("numerocpu").innerHTML = numCPU;
+let numPlayer = prompt("Inserisci il numero! (da 0 a 9)");
+document.getElementById("numeroutente").innerHTML = numPlayer;
+
+if (numPlayer == numCPU){
+    document.getElementById("risultato").innerHTML = "Hai vinto!";
+    document.getElementById("risultato").classList.add("text-success");
+}
+
+else {
+    document.getElementById("risultato").innerHTML = "Hai perso :C";
+    document.getElementById("risultato").classList.add("text-danger");
+    
+}
+
+
 
 
 
