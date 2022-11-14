7abe05843d6d766771667ec339c7b8fba59e4220
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-14 14:36:45 +0100

    Creato e finito snack1, creato snack2

---
+++ jsnack1/script.js
@@ -0,0 +1,16 @@
+let numero1 = prompt("Inserisci il primo numero!");
+let numero2 = prompt("inserisci il secondo numero");
+
+    
+if (numero1 > numero2){
+    console.log(numero1);
+}
+
+else if (numero2 > numero1){
+    console.log(numero2);
+}
+
+else if (numero1 == numero2){
+
+    console.log("I numeri sono uguali, scegli altri numeri!");
+}
