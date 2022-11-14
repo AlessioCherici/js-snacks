f20b62d7e27a4523d16243fe66d48b42d2f40126
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-14 17:38:25 +0100

    finito snack5 - creato snack6

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,18 +1,19 @@
-let arrayNumeri[];
+let arrayNumeri = [];
 
-for(let i = 0; i < 10 ; i++){
+for(let i = 0; i < 6 ; i++){
     do{
      arrayNumeri[i] = prompt("Inserisci i tuoi numeri!");
      arrayNumeri[i] = parseInt(arrayNumeri[i]);
     } 
     while(isNaN(arrayNumeri[i]))  
      arrayNumeri[i] = parseInt(arrayNumeri[i]);
-
     }
 
-    let dispari = arrayNumeri.filter(number => {
-        return number % 2 !== 0;
+    let dispari = arrayNumeri.filter(arrayNumeri => {
+        return arrayNumeri % 2 !== 0;
       });
+
+      console.log(dispari);
 
 
 
